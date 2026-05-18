11ac629e2225a086602945552ca2ccb69be0e90d
qianchen <chen.qian@dningbo.com>
2026-05-18 11:03:41 +0800

    company update

---
+++ src/main/java/com/sug/project/model/dto/questionsubmit/QuestionSubmitQueryRequest.java
@@ -0,0 +1,32 @@
+package com.sug.project.model.dto.questionsubmit;
+
+import com.sug.project.common.PageRequest;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+
+/**
+ * 题目提交查询请求
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class QuestionSubmitQueryRequest extends PageRequest implements Serializable {
+
+    /**
+     * 题目 id
+     */
+    private Long questionId;
+
+    /**
+     * 编程语言
+     */
+    private String language;
+
+    /**
+     * 用户账号
+     */
+    private String userAccount;
+
+    private static final long serialVersionUID = 1L;
+}
